c4b69df2fb4261467830cf2299a8df0373e8c410
danhanson <danielghanson93@gmail.com>
2015-09-20 15:15:58 -0400

    rest with vehicles without error checking works

--- app/io/pathfinder/models/CrudCompanion.scala
+++ app/io/pathfinder/models/CrudCompanion.scala
@@ -1,7 +1,7 @@
 package io.pathfinder.models
 import com.avaje.ebean.Model
 import com.avaje.ebean.Model.Finder
-import play.api.libs.json.Format
+import play.api.libs.json.{Format,Reads}
 
 /**
  * @author hansondg
@@ -10,4 +10,16 @@ trait CrudCompanion[K,V <: Model]{
     def finder: Finder[K,V]
     /*implicit def reads: Reads[V]*/
     implicit def format: Format[V]
+    final implicit def updateReads: Reads[_ <: Update[V]] = Update.reads
+
+    def create(): V
+    def Update: UpdateCompanion[V,_ <: Update[V]]
+
+    trait UpdateCompanion[V <: Model,U <: Update[V]] {
+        implicit def reads: Reads[U]
+    }
+    
+    trait Update[M <: Model] {
+        def apply(model: M): Unit
+    }
 }
--- app/io/pathfinder/models/Vehicle.scala
+++ app/io/pathfinder/models/Vehicle.scala
@@ -3,8 +3,7 @@ package io.pathfinder.models
 import com.avaje.ebean.Model
 import javax.persistence.{Id,Entity,GeneratedValue,GenerationType,Transient}
 import play.data.validation.Constraints.Required
-import play.api.libs.json.{Json,Format}
-import play.api.libs.functional.syntax._
+import play.api.libs.json.{Format,Reads,Json,JsValue}
 import javax.inject.Inject
 
 /**
@@ -12,9 +11,30 @@ import javax.inject.Inject
  */
 
 object Vehicle extends CrudCompanion[Long,Vehicle]{
+    
+    override val format: Format[Vehicle] = Json.format[Vehicle]
+
     override val finder: Model.Finder[Long,Vehicle] = new Model.Finder[Long,Vehicle](classOf[Vehicle])
-    override val format: Format[Vehicle] = null
-    def apply(id: Int, latitude: Double, longitude: Double, capacity: Int): Vehicle = {
+
+    override object Update extends UpdateCompanion[Vehicle,Update]{
+        override val reads = Json.reads[Update]
+    }
+
+    case class Update(
+        latitude:  Option[Double],
+        longitude: Option[Double],
+        capacity:  Option[Int]
+    ) extends super.Update[Vehicle] {
+        override def apply(v: Vehicle){
+            latitude.map  { v.latitude  = _ }
+            longitude.map { v.longitude = _ }
+            capacity.map  { v.capacity  = _ }
+        }
+    }
+
+    override def create() = new Vehicle
+
+    def apply(id: Long, latitude: Double, longitude: Double, capacity: Int): Vehicle = {
         val v = new Vehicle
         v.id = id
         v.latitude = latitude
@@ -22,6 +42,8 @@ object Vehicle extends CrudCompanion[Long,Vehicle]{
         v.capacity = capacity
         return v
     }
+
+    def unapply(v: Vehicle): Option[(Long, Double, Double, Int)] = Some((v.id, v.latitude, v.longitude, v.capacity))
 }
 
 @Entity
