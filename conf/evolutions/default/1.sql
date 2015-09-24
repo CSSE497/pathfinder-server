bf698f66ee05d65d74b5045c6a8f0b76c4917818
danhanson <danielghanson93@gmail.com>
2015-09-24 09:51:21 -0400

    added evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,36 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table commodity (
+  id                        bigint auto_increment not null,
+  startLatitude             double not null,
+  startLongitude            double not null,
+  endLatitude               double not null,
+  endLongitude              double not null,
+  param                     integer,
+  constraint pk_commodity primary key (id))
+;
+
+create table vehicle (
+  id                        bigint auto_increment not null,
+  latitude                  double not null,
+  longitude                 double not null,
+  capacity                  integer not null,
+  constraint pk_vehicle primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists commodity;
+
+drop table if exists vehicle;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
