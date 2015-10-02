49ad1b83793c80c96ac835dbd6fee1be4ae39a76
danhanson <danielghanson93@gmail.com>
2015-10-01 21:56:14 -0400

    removed index from application

--- app/io/pathfinder/controllers/Application.scala
+++ app/io/pathfinder/controllers/Application.scala
@@ -1,16 +1,11 @@
 package io.pathfinder.controllers
 
-import io.pathfinder.models.Model
 import io.pathfinder.websockets.{WebSocketActor, WebSocketMessage}
 import play.api.mvc.{WebSocket, Controller, Action}
 
-class ViewController extends Controller {
+class Application extends Controller {
   import io.pathfinder.websockets.WebSocketMessage.frameFormat
   import play.api.Play.current
-
-  def index = Action {
-    Ok("Pathfinder Webservice")
-  }
 
   def socket = WebSocket.acceptWithActor[WebSocketMessage,WebSocketMessage] {
     request => out =>
