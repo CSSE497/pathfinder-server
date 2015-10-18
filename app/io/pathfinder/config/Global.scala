47e1ccd60e51748ab7076aa7d7392bf1c0d8eac5
danhanson <danielghanson93@gmail.com>
2015-10-17 23:20:16 -0400

    changed Global into scala so that ActorSystem works correctly

---
+++ app/io/pathfinder/config/Global.scala
@@ -0,0 +1,25 @@
+package io.pathfinder.config
+
+import akka.actor.ActorSystem
+import io.pathfinder.routing.Router
+import play.api.GlobalSettings
+import play.api.Application
+import play.Logger
+
+/**
+ * These hooks are called by Play Framework. We can use them to initialize expensive objects and
+ * set up any communication channels.
+ */
+object Global extends GlobalSettings {
+
+    val actorSystem = ActorSystem.create()
+
+    override def onStart(app: Application) {
+        Logger.info("Application has started.")
+        Router.init()
+    }
+
+    override def onStop(app: Application) {
+        Logger.info("Application has stopped.")
+    }
+}
