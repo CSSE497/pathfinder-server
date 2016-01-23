dc3ae953773c2e90e8e36fe93090d9ed26c29f71
Adam Michael <adam@ajmichael.net>
2016-01-23 16:00:39 -0500

    Removed HTTP. We only support web sockets.

--- app/io/pathfinder/controllers/CommodityController.scala
+++
@@ -1,8 +0,0 @@
-package io.pathfinder.controllers
-
-import io.pathfinder.models.Commodity
-import io.pathfinder.websockets.ModelTypes
-
-class CommodityController extends CrudController[Long,Commodity](Commodity.Dao){
-    override val model = ModelTypes.Commodity
-}
--- test/io/pathfinder/controllers/ControllerTest.scala
+++
@@ -1,7 +0,0 @@
-package io.pathfinder.controllers
-
-import org.scalatest.MustMatchers
-import org.scalatestplus.play.PlaySpec
-import play.api.test.{DefaultAwaitTimeout, FutureAwaits}
-
-abstract class ControllerTest extends PlaySpec with MustMatchers with FutureAwaits with DefaultAwaitTimeout
