159782fa46ccb2cbac26e5278b69f9b2cda121ac
root <root@apiserver.c.phonic-aquifer-105721.internal>
2016-01-26 04:44:00 +0000

    added default app id and log messages for authentication

--- app/io/pathfinder/controllers/Application.scala
+++ app/io/pathfinder/controllers/Application.scala
@@ -3,6 +3,7 @@ package io.pathfinder.controllers
 import akka.actor.{Props, ActorRef}
 import io.pathfinder.websockets.{WebSocketActor, WebSocketMessage}
 import play.api.mvc.{Results, RequestHeader, WebSocket, Controller}
+import play.Logger
 
 import scala.concurrent.Future
 
@@ -17,7 +18,13 @@ class Application extends Controller {
 
     def socket = WebSocket.tryAcceptWithActor[WebSocketMessage,WebSocketMessage] { request: RequestHeader =>
         Future.successful(
-            request.headers.get("Authorization").orElse(request.cookies.get("AppId").map(_.value)).flatMap(
+            request.headers.get("Authorization").map{
+                h => Logger.info("Connection using id: " + h + ", from authorization header"); h
+            }.orElse(
+                request.cookies.get("AppId").map{
+                    c =>  Logger.info("Connection using id: " + c.value + ", from cookie"); c.value
+                }
+            ).orElse{Logger.info("Using default id: 7d8f2ead-ee48-45ef-8314-3c5bebd4db82"); Some("7d8f2ead-ee48-45ef-8314-3c5bebd4db82")}.flatMap(
                 appId =>
                     Option(io.pathfinder.models.Application.finder.byId(appId)).map(
                         app => Right(handlerProps(appId))
